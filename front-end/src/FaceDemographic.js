44383ad7974d5532b0e330229bd3457a88776612
dihim <ihimdavid@gmail.com>
2021-02-21 14:29:30 -0600

    some http issue?

--- front-end/src/FaceDemographic.js
+++ front-end/src/FaceDemographic.js
@@ -1,53 +1,23 @@
 import React from 'react';
-import axios from 'axios';
+import {
+  Container,
+  Grid,
+  makeStyles
+} from '@material-ui/core';
 
-export default class FaceDemographic extends React.Component {
-  state = {
-    urls: '',
+const useStyles = makeStyles(() => ({
+  root: {
   }
+}));
 
-  handleChange = async event => {
-    this.setState({ urls: event.target.value });
-  }
+const FaceDemographic = () => {
+  const classes = useStyles();
 
-  handleSubmit = async event => {
-    event.preventDefault();
-    console.log("test")/*
-    const payload = {
-      urls: [this.state.urls]
-    };
-    console.log(payload)
-    axios.post(`http://ec2-34-201-161-82.compute-1.amazonaws.com//predict`, payload)
-      .then(res => {
-        console.log("in then")
-        console.log(res);
-        console.log(res.data);
-      })
-      */
-     const json = JSON.stringify({ urls: [this.state.urls] });
-      const res = await axios.post('http://ec2-34-201-161-82.compute-1.amazonaws.com/predict', json, {
-      headers: {
-        // Overwrite Axios's automatically set Content-Type
-        'Content-Type': 'application/json'
-       }
-      });
-      console.log(res)
+  return (
+    <div>
 
-  }
-  
+    </div>
+  );
+};
 
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name:
-            <input type="text" name="url" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Get Demographics</button>
-        </form>
-      </div>
-    )
-  }
-}
+export default FaceDemographic;
---
+++ front-end/src/FaceDemographicNew.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class FaceDemographic extends React.Component {
+  state = {
+    urls: '',
+  }
+
+  handleChange = async event => {
+    this.setState({ urls: event.target.value });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    console.log("test")/*
+    const payload = {
+      urls: [this.state.urls]
+    };
+    console.log(payload)
+    axios.post(`http://ec2-34-201-161-82.compute-1.amazonaws.com//predict`, payload)
+      .then(res => {
+        console.log("in then")
+        console.log(res);
+        console.log(res.data);
+      })
+      */
+     const json = JSON.stringify({ urls: [this.state.urls] });
+      const res = await axios.post('http://ec2-34-201-161-82.compute-1.amazonaws.com/predict', json, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        'Content-Type': 'application/json'
+       }
+      });
+      console.log(res)
+
+  }
+  
+
+
+  render() {
+    return (
+      <div>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="url" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Get Demographics</button>
+        </form>
+      </div>
+    )
+  }
+}
