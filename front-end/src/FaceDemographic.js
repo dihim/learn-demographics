04ac45b4146644541a30f2597ad977972bd3c1fa
dihim <ihimdavid@gmail.com>
2021-02-21 22:51:38 -0600

    added pie chart and bar for age and gender

--- front-end/src/FaceDemographic.js
+++ front-end/src/FaceDemographic.js
@@ -2,8 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import {useSnackbar} from "notistack"
 import Chart from "react-apexcharts";
-import 'react-image-crop/dist/ReactCrop.css';
-import ReactCrop from 'react-image-crop';
 import {
   Card,
   Box,
@@ -23,23 +21,115 @@ const useStyles = makeStyles(() => ({
 const FaceDemographic = () => {
   const classes = useStyles();
   const {enqueueSnackbar} = useSnackbar();
+  const [genderDistribution, setGenderDistribution] = React.useState([0,0])
+  const [ageDistribution, setAgeDistribution] = React.useState([0, 0, 0, 0, 0, 0, 0, 0])
   const [prediction, setPrediction] = React.useState([])
   const [loadingPrediction, setLoadingPrediction] = React.useState(false)
   const [url, setUrl] = React.useState("")
- const options = {
-    chart: {
-      id: "basic-bar"
+  const piestate = {
+    series: genderDistribution,
+    options: {
+      chart: {
+        width: '100%',
+        type: 'pie',
+      },
+      labels: ["Male", "Female"],
+      theme: {
+        monochrome: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        pie: {
+          dataLabels: {
+            offset: -5
+          }
+        }
+      },
+      title: {
+        text: "Gender"
+      },
+      dataLabels: {
+        formatter(val, opts) {
+          const name = opts.w.globals.labels[opts.seriesIndex]
+          return [name, val.toFixed(1) + '%']
+        }
+      },
+      legend: {
+        show: false
+      }
     },
-    xaxis: {
-      categories: ["-13","13-17","18-24","25-34","35-44","45-54","54-64","65+"]
+  
+  
+  };
+ 
+  var options = {
+    series: [{
+    name: 'Inflation',
+    data: ageDistribution
+  }],
+    chart: {
+    height: 350,
+    type: 'bar',
+  },
+  plotOptions: {
+    bar: {
+      dataLabels: {
+        position: 'top', // top, center, bottom
+      },
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val;
+    },
+    offsetY: -20,
+    style: {
+      fontSize: '12px',
+      colors: ["#304758"]
+    }
+  },
+  
+  xaxis: {
+    categories: ["-12","13-17","18-24","25-34","35-44","45-54","54-64","65+"],
+    position: 'bottom',
+    axisBorder: {
+      show: true
+    },
+    axisTicks: {
+      show: true
+    },
+        tooltip: {
+      enabled: false,
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: true
+    },
+    axisTicks: {
+      show: false,
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val
+      }
+    }
+  
+  },
+  title: {
+    text: 'Age',
+    floating: true,
+    offsetY: 0,
+    align: 'center',
+    style: {
+      color: '#444'
     }
   }
-  const series = [
-    {
-      name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91]
-    }
-  ]
+  };
+
   function ageBuckets(x){
     if (x >= 0 && x < 13) return "12 and Under"
     if (x >= 13 && x < 18) return "13-17"
@@ -51,12 +141,35 @@ const FaceDemographic = () => {
     if (x >= 65) return "65+"
   }
 
+  function calcStats(predictions) {
+    console.log("see")
+    let ageRanges = [0, 0, 0, 0, 0, 0, 0, 0]
+    let gender = [0, 0]
+    predictions.forEach(stat => {
+      let x = stat.age
+      if (stat.gender == "Man") {
+        gender[0] = gender[0] + 1
+      } else {
+        gender[1] = gender[1] + 1
+      }
+      if (x >= 0 && x < 13) ageRanges[0] = ageRanges[0] + 1
+      if (x >= 13 && x < 18) ageRanges[1] = ageRanges[1] + 1
+      if (x >= 18 && x < 25) ageRanges[2] = ageRanges[2] + 1
+      if (x >= 25 && x < 35) ageRanges[3] = ageRanges[3] + 1
+      if (x >= 35 && x < 45) ageRanges[4] = ageRanges[4] + 1
+      if (x >= 45 && x < 54) ageRanges[5] = ageRanges[5] + 1
+      if (x >= 54 && x < 64) ageRanges[6] = ageRanges[6] + 1
+      if (x >= 65) ageRanges[7] = ageRanges[7] + 1
+    })
+    setAgeDistribution(ageRanges)
+    setGenderDistribution(gender)
+  }
+
   function displayFaces(){
     return prediction.map((face) => (
         <Card style={{margin: "1em"}} >
           <Box minWidth="150px" flexDirection="column" flexWrap="wrap"  minHeight="200px">
           <Box width="150px" height="150px">
-            <ReactCrop src={url} crop={{unit: 'px', x: 12, y: 50, width: 12, height: 13}} />
           </Box>
           <Box display="flex" flexDirection="row" justifyContent="space-evenly" alignItems="center">
             <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
@@ -91,6 +204,7 @@ const FaceDemographic = () => {
       console.log(res)
       enqueueSnackbar("Image Classifed",{variant: 'success'})
       setPrediction(res.data)
+      calcStats(res.data)
       setLoadingPrediction(false)
     }
   } catch(err) {
@@ -130,30 +244,23 @@ const FaceDemographic = () => {
           </Button>
         </Box>
       </Card>
-      <Card style={{marginLeft: "2em", display: "flex", flexDirection: "column", flexWrap:"wrap", overflow:"scroll"}}>
-        <Card  style={{width: "500px", height: "75px", margin: "1em"}} >
+      <Card style={{marginLeft: "2em", minWidth:"500px", display: "flex", flexDirection: "column", flexWrap:"wrap", overflow:"scroll"}}>
           <Box display="flex" flexDirection="row" justifyContent="space-evenly" alignItems="center">
             {(!loadingPrediction) ? 
             <React.Fragment>
-            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-              <Typography variant="h5">
-              Gender
-              </Typography>
-              <Chart
-                options={options}
-                series={series}
-                type="bar"
+               <Chart
+                options={piestate.options}
+                series={piestate.series}
+                type="pie"
                 width="200"
               />
-            </Box>
-            <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-              <Typography variant="h5">
-              Age
-              </Typography>
-              <Typography variant="h6">
-              {ageBuckets(prediction.age)}
-              </Typography>
-            </Box>
+               <Chart
+                options={options}
+                series={options.series}
+                type="bar"
+                width="300"
+                height="200"
+              />
             </React.Fragment> 
             :
             <Box mt={"1em"}>
@@ -161,12 +268,13 @@ const FaceDemographic = () => {
             </Box>
             }
           </Box>
-        </Card>
+       
       <Box  flexWrap="wrap" maxWidth="500px" maxHeight="490px" overflowy="scroll"  display="flex" flexDirection="row" justifyContent="center" alignItems="center">
       {(!loadingPrediction && prediction[0] ) ? 
         displayFaces() :
         ""
       }
+     
       </Box>
 
       </Card>
