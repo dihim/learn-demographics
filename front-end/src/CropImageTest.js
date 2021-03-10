7455015d759921f979eb35318fe96c28f7994b4b
lilseyi <40449263+lilseyi@users.noreply.github.com>
2021-03-10 16:16:27 -0500

    im mentally scarred

---
+++ front-end/src/CropImageTest.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import 'react-image-crop/dist/ReactCrop.css';
+import {
+  makeStyles,
+  Paper,
+  Box,
+  Avatar,
+  IconButton
+} from '@material-ui/core';
+
+
+const getCroppedImage = (url,bbox) => {
+    var neededWidth = 300
+    var scale = neededWidth/parseInt(bbox.w)
+    var divStyle = {
+        position: "relative",
+        width: `${neededWidth}px`,
+        height: `${neededWidth}px`,
+        overflow: "hidden",
+        //transform: `scale(${scale}) translate(${neededWidth/2 }px, ${neededWidth/2 }px)`,
+        
+      }
+    var imgStyle = {
+        position: "absolute",
+        transform: `scale(${scale})`,
+        margin: `-${bbox.y}px 0 0 -${bbox.x}px`,
+    }
+    return (
+        
+              <div style={divStyle}>
+                    <img src={url} style={imgStyle}/>
+                </div>
+
+    );
+};
+
+export default getCroppedImage;
